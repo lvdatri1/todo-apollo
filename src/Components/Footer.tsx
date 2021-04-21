3277000d73f3de5c5e54fd374718e52541faa89c
Trinh <t.le@hpa.org.nz>
2021-04-21 15:15:53 +1200

    first app

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <b>Here is the Footer of our App</b>
+            </div>
+        )
+    }
+}
---
+++ src/Components/MainToDos.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainToDos extends Component {
+    render() {
+        return (
+            <div>
+                Main todos
+            </div>
+        )
+    }
+}
