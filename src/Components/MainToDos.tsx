1cd8b6a52c7cec7117e9b9153fd810bb0a0f57f4
Trinh Le <lvdatri@gmail.com>
2021-04-21 22:30:53 +1200

    added app works

--- src/Components/MainToDos.tsx
+++ src/Components/MainToDos.tsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import {AllToDos} from '../Operation/GetToDos';
 
-export default class MainToDos extends Component {
-constructor(props: any){
+interface PropsType{}
+interface StateType{
+list:string[]
+}
+export default class MainToDos extends Component<PropsType, StateType> {
+constructor(props: any, state: StateType){
     super(props);
 this._onClickButton=this._onClickButton.bind(this);
+this.setState({list:[]});
+this._renderList=this._renderList.bind(this);
 }
 _onClickButton(){
     var x: string[];
     x=AllToDos;
-
+this.setState({list:x});
     console.log(x);
+}
+_renderList(){
+    if(this.state==null) return <div>nolist</div>
+    const todoItems = this.state.list.map((item: string, index: number) =>
+  <li key={index}>
+    {item}
+  </li>);
+  return todoItems;
 }
     render() {
 
@@ -18,6 +32,7 @@ _onClickButton(){
             <div>
                 Main todos
              <button onClick={this._onClickButton}>Click here to have information</button>
+           <br/>{this._renderList()}
             </div>
         )
     }
